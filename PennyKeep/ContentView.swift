676232f92be9308e721103debc8762f7cf3b5b48
sota-mitsumori <sota.mitsumori@gmail.com>
2025-02-07 08:19:36 +0900

    Initial Commit

---
+++ PennyKeep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PennyKeep
+//
+//  Created by Sota Mitsumori on 2025/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PennyKeep/PennyKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyKeepApp.swift
+//  PennyKeep
+//
+//  Created by Sota Mitsumori on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct PennyKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
