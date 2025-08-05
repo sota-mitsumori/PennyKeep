c072a492cb8e2e0f70b8f99b73277846b40fd45b
sota-mitsumori <sota.mitsumori@gmail.com>
2025-08-05 15:54:01 +0900

    v1.3.0

--- PennyKeep/Settings/SettingsView.swift
+++ PennyKeep/Settings/SettingsView.swift
@@ -15,7 +15,7 @@ struct SettingsView: View {
                     .pickerStyle(.automatic)
                 }
                 Section(header: Text("Version")) {
-                    Text("Version 1.3.0-beta (2025.05.15)")
+                    Text("Version 1.3.0 (2025.08.05)")
                 }
             }
             .navigationTitle("Settings")
